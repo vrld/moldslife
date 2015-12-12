a9392fd14ef2a1be5d026a99e98a67f0627099b1
Matthias Richter <vrld@vrld.org>
2015-12-12 17:21:53 +0100

    Menu placeholder

--- states/menu.lua
+++ states/menu.lua
@@ -4,11 +4,19 @@ function st:init()
 end
 
 function st:enter()
+	love.graphics.setBackgroundColor(30,28,25)
+	love.graphics.setFont(Font[50])
 end
 
 function st:draw()
-	love.graphics.setColor(100,100,100,100)
+	love.graphics.setColor(100,100,100)
 	love.graphics.print("Grow and 2 Buttons", 10,10)
+
+	love.graphics.printf("-press any key-", 0,HEIGHT-120,WIDTH, 'center')
+end
+
+function st:keypressed()
+	GS.switch(State.game)
 end
 
 return st
